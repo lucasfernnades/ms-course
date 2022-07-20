1c081e41e02147e6f1c8077cdc4f3e93fc61ecf1
Lucas Fernandes <lucas.fernandes97@hotmail.com>
2022-07-20 16:36:44 -0300

    Project hr-api-gateaway-zuul created

---
+++ hr-api-gateaway-zuul/src/test/java/com/lks/hrapigateawayzuul/HrApiGateawayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lks.hrapigateawayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGateawayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
