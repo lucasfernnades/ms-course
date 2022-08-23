c9ac0e4afca1eb684de560fe9757d20aaf1e0fd2
Lucas Fernandes <lucas.fernandes97@hotmail.com>
2022-08-23 12:24:36 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/lks/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lks.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lks.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
